b3664a166267b0ae55672d34ea2476b28dff8f39
tzushimelon <4021761+tzushimelon@users.noreply.github.com>
2022-12-04 20:58:03 -0800

    Return all eligible add/remove level roles instead of only at level up

--- data/schema.sql
+++ data/schema.sql
@@ -941,6 +941,145 @@ COMMENT ON FUNCTION app_public.timeframe_user_levels(timeframe app_hidden.level_
 
 
 --
+-- Name: update_user_xp(bigint, bigint, bigint); Type: FUNCTION; Schema: app_public; Owner: -
+--
+
+CREATE FUNCTION app_public.update_user_xp(guild_id bigint, channel_id bigint, user_id bigint) RETURNS app_public.user_xp_update_result
+    LANGUAGE plpgsql SECURITY DEFINER
+    SET search_path TO 'pg_catalog', 'public', 'app_public', 'pg_temp'
+    AS $_$
+#variable_conflict use_column
+declare
+  old_level bigint;
+  new_level bigint;
+  new_last_msg timestamp;
+
+  -- level roles to add/remove
+  add_role_ids    bigint[];
+  remove_role_ids bigint[];
+begin
+  -- Ignore any channels or roles that are blocked
+  if exists (
+    select from app_public.xp_blocks
+      where 
+        guild_id = $1
+        and
+        (
+          block_id = $2
+          or
+          block_id = any($4)
+        )
+  ) then
+    raise notice 'Ignoring XP gain in channel/role % in guild %', $2, $1;
+    return (old_level, new_level, '{}'::bigint[], '{}'::bigint[]);
+  end if;
+
+  insert into app_public.user_levels (
+    guild_id,
+    user_id,
+    msg_all_time,
+    msg_month,
+    msg_week,
+    msg_day,
+    last_msg
+  )
+    values ($1, $3, 5, 5, 5, 5, now())
+    on conflict (guild_id, user_id) do update
+      set last_msg = now(),
+      msg_all_time = app_public.user_levels.msg_all_time + 5,
+      msg_month = (
+        case
+          when extract(MONTH from app_public.user_levels.last_msg) = extract(MONTH from now())
+           and extract(YEAR  from app_public.user_levels.last_msg) = extract(YEAR  from now())
+            then app_public.user_levels.msg_month + 5
+          else 5
+        end
+      ),
+      msg_week = (
+        case
+          when extract(WEEK from app_public.user_levels.last_msg) = extract(WEEK from now())
+           and extract(YEAR from app_public.user_levels.last_msg) = extract(YEAR from now())
+            -- Fix using msg_week instead of msg_month
+            then app_public.user_levels.msg_week + 5
+          else 5
+        end
+      ),
+      msg_day = (
+        case
+          when extract(DAY  from app_public.user_levels.last_msg) = extract(DAY  from now())
+           and extract(YEAR from app_public.user_levels.last_msg) = extract(YEAR from now())
+            then app_public.user_levels.msg_day + 5
+          else 5
+        end
+      )
+      where app_public.user_levels.last_msg < (now() - interval '1 minute')
+    returning
+      level,
+      (select level
+        from app_public.user_levels
+        where guild_id = $1
+          and user_id = $3
+      ) as old_level
+      into
+      new_level,
+      old_level;
+
+  -- new_level will be null if the user was not updated.
+  -- If the user is updated, new_level could be the same as old_level if the
+  -- user did not level up
+  if new_level is null then
+    raise notice 'user % in guild % was not updated (already gained xp within last minute)', $3, $1;
+
+    return (old_level, new_level, '{}'::bigint[], '{}'::bigint[]);
+  end if;
+
+  raise notice 'added xp for member %: new_level %, old_level %', user_id, new_level, old_level;
+
+
+  -- Log level ups but still continue if user did not level up
+  if new_level != old_level then
+    raise notice 'user % in guild % leveled up from % to %', $3, $1, old_level, new_level;
+  end if;
+
+  -- Eligible roles
+  select
+    coalesce(array_agg(role_id), '{}')
+  into
+    add_role_ids
+  from app_public.level_roles
+    where
+      app_public.level_roles.guild_id = $1
+      and
+      app_public.level_roles.add_level is not null
+      and
+      app_public.level_roles.add_level = new_level
+      and
+      -- Don't add level roles that should be removed
+      (
+        app_public.level_roles.remove_level is null
+        or
+        app_public.level_roles.remove_level < new_level
+      );
+
+  -- Remove roles
+  select
+    coalesce(array_agg(role_id), '{}')
+  into
+    remove_role_ids
+  from app_public.level_roles
+    where
+      app_public.level_roles.guild_id = $1
+      and
+      app_public.level_roles.remove_level is not null
+      and
+      app_public.level_roles.remove_level < new_level;
+
+  return (old_level, new_level, add_role_ids, remove_role_ids);
+end;
+$_$;
+
+
+--
 -- Name: update_user_xp(bigint, bigint, bigint, bigint[]); Type: FUNCTION; Schema: app_public; Owner: -
 --
 
@@ -1024,7 +1163,9 @@ begin
       new_level,
       old_level;
 
-  -- new_level will be null if the user was not updated
+  -- new_level will be null if the user was not updated.
+  -- If the user is updated, new_level could be the same as old_level if the
+  -- user did not level up
   if new_level is null then
     raise notice 'user % in guild % was not updated (already gained xp within last minute)', $3, $1;
 
@@ -1033,14 +1174,12 @@ begin
 
   raise notice 'added xp for member %: new_level %, old_level %', user_id, new_level, old_level;
 
-  -- User did not level up, just return
-  if new_level = old_level then
-    return (old_level, new_level, '{}'::bigint[], '{}'::bigint[]);
+  -- Log level ups but still continue if user did not level up
+  if new_level != old_level then
+    raise notice 'user % in guild % leveled up from % to %', $3, $1, old_level, new_level;
   end if;
 
-  raise notice 'user % in guild % leveled up from % to %', $3, $1, old_level, new_level;
-
-  -- Add roles
+  -- Eligible roles
   select
     coalesce(array_agg(role_id), '{}')
   into
@@ -1051,9 +1190,17 @@ begin
       and
       app_public.level_roles.add_level is not null
       and
-      app_public.level_roles.add_level = new_level;
+      app_public.level_roles.add_level <= new_level
+      and
+      -- Don't add level roles that should be removed
+      (
+        app_public.level_roles.remove_level is null
+        or
+        app_public.level_roles.remove_level > new_level
+      );
 
-  -- Remove roles
+  -- Remove roles, do not need to check for if add_level is < since remove_level
+  -- should be greater than add_level
   select
     coalesce(array_agg(role_id), '{}')
   into
@@ -1064,7 +1211,7 @@ begin
       and
       app_public.level_roles.remove_level is not null
       and
-      app_public.level_roles.remove_level = new_level;
+      app_public.level_roles.remove_level < new_level;
 
   return (old_level, new_level, add_role_ids, remove_role_ids);
 end;
@@ -2452,6 +2599,15 @@ GRANT ALL ON FUNCTION app_public.set_role_menu_role_order(guild_id bigint, menu_
 
 REVOKE ALL ON FUNCTION app_public.timeframe_user_levels(timeframe app_hidden.level_timeframe, guild_id bigint) FROM PUBLIC;
 GRANT ALL ON FUNCTION app_public.timeframe_user_levels(timeframe app_hidden.level_timeframe, guild_id bigint) TO sushii_visitor;
+
+
+--
+-- Name: FUNCTION update_user_xp(guild_id bigint, channel_id bigint, user_id bigint); Type: ACL; Schema: app_public; Owner: -
+--
+
+REVOKE ALL ON FUNCTION app_public.update_user_xp(guild_id bigint, channel_id bigint, user_id bigint) FROM PUBLIC;
+GRANT ALL ON FUNCTION app_public.update_user_xp(guild_id bigint, channel_id bigint, user_id bigint) TO sushii_visitor;
+GRANT ALL ON FUNCTION app_public.update_user_xp(guild_id bigint, channel_id bigint, user_id bigint) TO sushii_admin;
 
 
 --

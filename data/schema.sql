c2d62f8e6f5fdc2af5b94d82cdcf4bd647b44ae6
tzushimelon <4021761+tzushimelon@users.noreply.github.com>
2023-01-29 16:21:25 -0800

    Add migration for mod log search and bulk edit

--- data/schema.sql
+++ data/schema.sql
@@ -52,6 +52,13 @@ CREATE SCHEMA app_public;
 
 
 --
+-- Name: postgraphile_watch; Type: SCHEMA; Schema: -; Owner: -
+--
+
+CREATE SCHEMA postgraphile_watch;
+
+
+--
 -- Name: pg_trgm; Type: EXTENSION; Schema: -; Owner: -
 --
 
@@ -613,6 +620,45 @@ $_$;
 
 
 --
+-- Name: mod_logs; Type: TABLE; Schema: app_public; Owner: -
+--
+
+CREATE TABLE app_public.mod_logs (
+    guild_id bigint NOT NULL,
+    case_id bigint NOT NULL,
+    action text NOT NULL,
+    action_time timestamp without time zone NOT NULL,
+    pending boolean NOT NULL,
+    user_id bigint NOT NULL,
+    user_tag text NOT NULL,
+    executor_id bigint,
+    reason text,
+    msg_id bigint,
+    attachments text[] DEFAULT '{}'::text[] NOT NULL
+);
+
+
+--
+-- Name: bulk_update_mod_log_reason(bigint, bigint, bigint, bigint, text); Type: FUNCTION; Schema: app_public; Owner: -
+--
+
+CREATE FUNCTION app_public.bulk_update_mod_log_reason(guild_id bigint, start_case_id bigint, end_case_id bigint, executor_id bigint, reason text) RETURNS SETOF app_public.mod_logs
+    LANGUAGE sql STRICT
+    AS $_$
+  update app_public.mod_logs
+    set
+      reason = $5,
+      executor_id = $4
+  where guild_id = $1
+    -- ignore pending cases
+    and pending = false
+    and case_id >= $2 -- start_case_id
+    and case_id <= $3 -- end_case_id
+  returning *;
+$_$;
+
+
+--
 -- Name: current_session_id(); Type: FUNCTION; Schema: app_public; Owner: -
 --
 
@@ -867,6 +913,27 @@ $$;
 
 
 --
+-- Name: search_mod_logs(bigint, bigint, integer); Type: FUNCTION; Schema: app_public; Owner: -
+--
+
+CREATE FUNCTION app_public.search_mod_logs(guild_id bigint, search_case_id bigint, max_results integer DEFAULT 25) RETURNS SETOF app_public.mod_logs
+    LANGUAGE sql IMMUTABLE STRICT
+    AS $_$
+  select *
+    from app_public.mod_logs
+  where guild_id = $1
+    -- ignore pending cases
+    and pending = false
+    -- starts_with search for case_id
+    and case_id::text like $2 || '%'
+  order by
+    -- sort by case_id desc so that the most recent case is first
+    case_id desc
+  limit $3;
+$_$;
+
+
+--
 -- Name: set_role_menu_role_order(bigint, text, bigint[]); Type: FUNCTION; Schema: app_public; Owner: -
 --
 
@@ -938,145 +1005,6 @@ $$;
 --
 
 COMMENT ON FUNCTION app_public.timeframe_user_levels(timeframe app_hidden.level_timeframe, guild_id bigint) IS 'Leaderboard for given timeframe and optional guild. If guild is null, it is the global leaderboard';
-
-
---
--- Name: update_user_xp(bigint, bigint, bigint); Type: FUNCTION; Schema: app_public; Owner: -
---
-
-CREATE FUNCTION app_public.update_user_xp(guild_id bigint, channel_id bigint, user_id bigint) RETURNS app_public.user_xp_update_result
-    LANGUAGE plpgsql SECURITY DEFINER
-    SET search_path TO 'pg_catalog', 'public', 'app_public', 'pg_temp'
-    AS $_$
-#variable_conflict use_column
-declare
-  old_level bigint;
-  new_level bigint;
-  new_last_msg timestamp;
-
-  -- level roles to add/remove
-  add_role_ids    bigint[];
-  remove_role_ids bigint[];
-begin
-  -- Ignore any channels or roles that are blocked
-  if exists (
-    select from app_public.xp_blocks
-      where 
-        guild_id = $1
-        and
-        (
-          block_id = $2
-          or
-          block_id = any($4)
-        )
-  ) then
-    raise notice 'Ignoring XP gain in channel/role % in guild %', $2, $1;
-    return (old_level, new_level, '{}'::bigint[], '{}'::bigint[]);
-  end if;
-
-  insert into app_public.user_levels (
-    guild_id,
-    user_id,
-    msg_all_time,
-    msg_month,
-    msg_week,
-    msg_day,
-    last_msg
-  )
-    values ($1, $3, 5, 5, 5, 5, now())
-    on conflict (guild_id, user_id) do update
-      set last_msg = now(),
-      msg_all_time = app_public.user_levels.msg_all_time + 5,
-      msg_month = (
-        case
-          when extract(MONTH from app_public.user_levels.last_msg) = extract(MONTH from now())
-           and extract(YEAR  from app_public.user_levels.last_msg) = extract(YEAR  from now())
-            then app_public.user_levels.msg_month + 5
-          else 5
-        end
-      ),
-      msg_week = (
-        case
-          when extract(WEEK from app_public.user_levels.last_msg) = extract(WEEK from now())
-           and extract(YEAR from app_public.user_levels.last_msg) = extract(YEAR from now())
-            -- Fix using msg_week instead of msg_month
-            then app_public.user_levels.msg_week + 5
-          else 5
-        end
-      ),
-      msg_day = (
-        case
-          when extract(DAY  from app_public.user_levels.last_msg) = extract(DAY  from now())
-           and extract(YEAR from app_public.user_levels.last_msg) = extract(YEAR from now())
-            then app_public.user_levels.msg_day + 5
-          else 5
-        end
-      )
-      where app_public.user_levels.last_msg < (now() - interval '1 minute')
-    returning
-      level,
-      (select level
-        from app_public.user_levels
-        where guild_id = $1
-          and user_id = $3
-      ) as old_level
-      into
-      new_level,
-      old_level;
-
-  -- new_level will be null if the user was not updated.
-  -- If the user is updated, new_level could be the same as old_level if the
-  -- user did not level up
-  if new_level is null then
-    raise notice 'user % in guild % was not updated (already gained xp within last minute)', $3, $1;
-
-    return (old_level, new_level, '{}'::bigint[], '{}'::bigint[]);
-  end if;
-
-  raise notice 'added xp for member %: new_level %, old_level %', user_id, new_level, old_level;
-
-
-  -- Log level ups but still continue if user did not level up
-  if new_level != old_level then
-    raise notice 'user % in guild % leveled up from % to %', $3, $1, old_level, new_level;
-  end if;
-
-  -- Eligible roles
-  select
-    coalesce(array_agg(role_id), '{}')
-  into
-    add_role_ids
-  from app_public.level_roles
-    where
-      app_public.level_roles.guild_id = $1
-      and
-      app_public.level_roles.add_level is not null
-      and
-      app_public.level_roles.add_level = new_level
-      and
-      -- Don't add level roles that should be removed
-      (
-        app_public.level_roles.remove_level is null
-        or
-        app_public.level_roles.remove_level < new_level
-      );
-
-  -- Remove roles
-  select
-    coalesce(array_agg(role_id), '{}')
-  into
-    remove_role_ids
-  from app_public.level_roles
-    where
-      app_public.level_roles.guild_id = $1
-      and
-      app_public.level_roles.remove_level is not null
-      and
-      app_public.level_roles.remove_level < new_level;
-
-  return (old_level, new_level, add_role_ids, remove_role_ids);
-end;
-$_$;
 
 
 --
@@ -1287,6 +1215,48 @@ CREATE FUNCTION app_public.user_guild_rank(guild_id bigint, user_id bigint) RETU
       ) t
   where t.user_id = user_id
   limit 1;
+$$;
+
+
+--
+-- Name: notify_watchers_ddl(); Type: FUNCTION; Schema: postgraphile_watch; Owner: -
+--
+
+CREATE FUNCTION postgraphile_watch.notify_watchers_ddl() RETURNS event_trigger
+    LANGUAGE plpgsql
+    AS $$
+begin
+  perform pg_notify(
+    'postgraphile_watch',
+    json_build_object(
+      'type',
+      'ddl',
+      'payload',
+      (select json_agg(json_build_object('schema', schema_name, 'command', command_tag)) from pg_event_trigger_ddl_commands() as x)
+    )::text
+  );
+end;
+$$;
+
+
+--
+-- Name: notify_watchers_drop(); Type: FUNCTION; Schema: postgraphile_watch; Owner: -
+--
+
+CREATE FUNCTION postgraphile_watch.notify_watchers_drop() RETURNS event_trigger
+    LANGUAGE plpgsql
+    AS $$
+begin
+  perform pg_notify(
+    'postgraphile_watch',
+    json_build_object(
+      'type',
+      'drop',
+      'payload',
+      (select json_agg(distinct x.schema_name) from pg_event_trigger_dropped_objects() as x)
+    )::text
+  );
+end;
 $$;
 
 
@@ -1563,25 +1533,6 @@ CREATE TABLE app_public.messages (
     created timestamp without time zone NOT NULL,
     content text NOT NULL,
     msg jsonb NOT NULL
-);
-
-
---
--- Name: mod_logs; Type: TABLE; Schema: app_public; Owner: -
---
-
-CREATE TABLE app_public.mod_logs (
-    guild_id bigint NOT NULL,
-    case_id bigint NOT NULL,
-    action text NOT NULL,
-    action_time timestamp without time zone NOT NULL,
-    pending boolean NOT NULL,
-    user_id bigint NOT NULL,
-    user_tag text NOT NULL,
-    executor_id bigint,
-    reason text,
-    msg_id bigint,
-    attachments text[] DEFAULT '{}'::text[] NOT NULL
 );
 
 
@@ -2467,6 +2418,22 @@ GRANT ALL ON FUNCTION app_public.add_role_menu_roles(guild_id bigint, menu_name 
 
 
 --
+-- Name: TABLE mod_logs; Type: ACL; Schema: app_public; Owner: -
+--
+
+GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE app_public.mod_logs TO sushii_admin;
+
+
+--
+-- Name: FUNCTION bulk_update_mod_log_reason(guild_id bigint, start_case_id bigint, end_case_id bigint, executor_id bigint, reason text); Type: ACL; Schema: app_public; Owner: -
+--
+
+REVOKE ALL ON FUNCTION app_public.bulk_update_mod_log_reason(guild_id bigint, start_case_id bigint, end_case_id bigint, executor_id bigint, reason text) FROM PUBLIC;
+GRANT ALL ON FUNCTION app_public.bulk_update_mod_log_reason(guild_id bigint, start_case_id bigint, end_case_id bigint, executor_id bigint, reason text) TO sushii_visitor;
+GRANT ALL ON FUNCTION app_public.bulk_update_mod_log_reason(guild_id bigint, start_case_id bigint, end_case_id bigint, executor_id bigint, reason text) TO sushii_admin;
+
+
+--
 -- Name: FUNCTION current_session_id(); Type: ACL; Schema: app_public; Owner: -
 --
 
@@ -2585,6 +2552,15 @@ GRANT ALL ON FUNCTION app_public.random_tag(guild_id bigint, query text, starts_
 
 
 --
+-- Name: FUNCTION search_mod_logs(guild_id bigint, search_case_id bigint, max_results integer); Type: ACL; Schema: app_public; Owner: -
+--
+
+REVOKE ALL ON FUNCTION app_public.search_mod_logs(guild_id bigint, search_case_id bigint, max_results integer) FROM PUBLIC;
+GRANT ALL ON FUNCTION app_public.search_mod_logs(guild_id bigint, search_case_id bigint, max_results integer) TO sushii_visitor;
+GRANT ALL ON FUNCTION app_public.search_mod_logs(guild_id bigint, search_case_id bigint, max_results integer) TO sushii_admin;
+
+
+--
 -- Name: FUNCTION set_role_menu_role_order(guild_id bigint, menu_name text, role_ids bigint[]); Type: ACL; Schema: app_public; Owner: -
 --
 
@@ -2599,24 +2575,6 @@ GRANT ALL ON FUNCTION app_public.set_role_menu_role_order(guild_id bigint, menu_
 
 REVOKE ALL ON FUNCTION app_public.timeframe_user_levels(timeframe app_hidden.level_timeframe, guild_id bigint) FROM PUBLIC;
 GRANT ALL ON FUNCTION app_public.timeframe_user_levels(timeframe app_hidden.level_timeframe, guild_id bigint) TO sushii_visitor;
-
-
---
--- Name: FUNCTION update_user_xp(guild_id bigint, channel_id bigint, user_id bigint); Type: ACL; Schema: app_public; Owner: -
---
-
-REVOKE ALL ON FUNCTION app_public.update_user_xp(guild_id bigint, channel_id bigint, user_id bigint) FROM PUBLIC;
-GRANT ALL ON FUNCTION app_public.update_user_xp(guild_id bigint, channel_id bigint, user_id bigint) TO sushii_visitor;
-GRANT ALL ON FUNCTION app_public.update_user_xp(guild_id bigint, channel_id bigint, user_id bigint) TO sushii_admin;
-
-
---
--- Name: FUNCTION update_user_xp(guild_id bigint, channel_id bigint, user_id bigint, role_ids bigint[]); Type: ACL; Schema: app_public; Owner: -
---
-
-REVOKE ALL ON FUNCTION app_public.update_user_xp(guild_id bigint, channel_id bigint, user_id bigint, role_ids bigint[]) FROM PUBLIC;
-GRANT ALL ON FUNCTION app_public.update_user_xp(guild_id bigint, channel_id bigint, user_id bigint, role_ids bigint[]) TO sushii_visitor;
-GRANT ALL ON FUNCTION app_public.update_user_xp(guild_id bigint, channel_id bigint, user_id bigint, role_ids bigint[]) TO sushii_admin;
 
 
 --
@@ -2916,13 +2874,6 @@ GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE app_public.messages TO sushii_admin;
 
 
 --
--- Name: TABLE mod_logs; Type: ACL; Schema: app_public; Owner: -
---
-
-GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE app_public.mod_logs TO sushii_admin;
-
-
---
 -- Name: TABLE msg_log_blocks; Type: ACL; Schema: app_public; Owner: -
 --
 
@@ -3062,6 +3013,23 @@ ALTER DEFAULT PRIVILEGES FOR ROLE sushii IN SCHEMA public GRANT SELECT,INSERT,DE
 --
 
 ALTER DEFAULT PRIVILEGES FOR ROLE sushii REVOKE ALL ON FUNCTIONS  FROM PUBLIC;
+
+
+--
+-- Name: postgraphile_watch_ddl; Type: EVENT TRIGGER; Schema: -; Owner: -
+--
+
+CREATE EVENT TRIGGER postgraphile_watch_ddl ON ddl_command_end
+         WHEN TAG IN ('ALTER AGGREGATE', 'ALTER DOMAIN', 'ALTER EXTENSION', 'ALTER FOREIGN TABLE', 'ALTER FUNCTION', 'ALTER POLICY', 'ALTER SCHEMA', 'ALTER TABLE', 'ALTER TYPE', 'ALTER VIEW', 'COMMENT', 'CREATE AGGREGATE', 'CREATE DOMAIN', 'CREATE EXTENSION', 'CREATE FOREIGN TABLE', 'CREATE FUNCTION', 'CREATE INDEX', 'CREATE POLICY', 'CREATE RULE', 'CREATE SCHEMA', 'CREATE TABLE', 'CREATE TABLE AS', 'CREATE VIEW', 'DROP AGGREGATE', 'DROP DOMAIN', 'DROP EXTENSION', 'DROP FOREIGN TABLE', 'DROP FUNCTION', 'DROP INDEX', 'DROP OWNED', 'DROP POLICY', 'DROP RULE', 'DROP SCHEMA', 'DROP TABLE', 'DROP TYPE', 'DROP VIEW', 'GRANT', 'REVOKE', 'SELECT INTO')
+   EXECUTE FUNCTION postgraphile_watch.notify_watchers_ddl();
+
+
+--
+-- Name: postgraphile_watch_drop; Type: EVENT TRIGGER; Schema: -; Owner: -
+--
+
+CREATE EVENT TRIGGER postgraphile_watch_drop ON sql_drop
+   EXECUTE FUNCTION postgraphile_watch.notify_watchers_drop();
 
 
 --
